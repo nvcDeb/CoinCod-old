4db540697665057aa42c81f9bb17e46186d9db8f
vincent <vincent@nexvend.com>
2012-06-09 01:23:43 +0800

    adjustment done for faq

--- FAQ/Faq_page3.php
+++ FAQ/Faq_page3.php
@@ -14,35 +14,29 @@
 		include "../template/templateheader.php";
 	?>
      <div id="content_container">
+	 <h1><a href="index.php"><img src="../template/template_image/header/faq.png"></a></h1>
     	<div class="auction_container">
 		
-		<h1><a href="index.php"><img src="../template/template_image/header/faq.png" border="0"></a></h1>
-		<div class="faq">
+		<div class="answer">
 		<ol>
         
-		<li><a name="Question11"><strong>Can I undo already submitted bids?</strong></a></li>
+		<li><a name="Question11">Can I undo already submitted bids?</a></li>
         No. Bids that were placed cannot be undone.
-        </br></br>
         
-		<li><a name="Question12"><strong>Can I bid for the same product more than once?</strong></a></li>
+		<li><a name="Question12">Can I bid for the same product more than once?</a></li>
         Yes you may. There are no limits to how many times bids can be placed on an auction.
-        </br></br>
         
-		<li><a name="Question13"><strong>Can I bid during the night?</strong></a></li>
+		<li><a name="Question13">Can I bid during the night?</a></li>
         All auctions are open for 24 hours. The opening time for each auction is specified on the website. Bids can also be bought 24hours everyday.
-        </br></br>
         
-		<li><a name="Question14"><strong>How do I know if I am the highest bidder?</strong></a></li>
+		<li><a name="Question14">How do I know if I am the highest bidder?</a></li>
         Your nickname will appear in the item auction box under the highest bidder column.
-        </br></br>
         
-		<li><a name="Question15"><strong>How do I know if an auction has ended?</strong></a></li>
+		<li><a name="Question15">How do I know if an auction has ended?</a></li>
         The bid button will be replaced with a red "ENDED" button and the countdown will be replaced with "0:00”.
-        </br></br>
 
 		</ol>
-		</div>  <!--end div Faq-->
-		<br/>        
+		</div>  <!--end div Faq-->    
       	</div>  <!--end div auction_container-->
 	</div>  <!--end div content_container-->
 </div>  <!--end div wrapper-->
