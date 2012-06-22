75759aba2f177f0e17cc32ba55611e49af02fd58
vincent <vincent@nexvend.com>
2012-06-22 22:08:46 +0800

    faq, templateheader, templatefooter path change, and remove image from about_us

--- Login_Form/check_login.php
+++ Login_Form/check_login.php
@@ -1,3 +1,6 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>	
@@ -54,7 +57,7 @@
 			} 
 			else 
 			{
-				include "login_form.php";
+				include("$_SERVER[DOCUMENT_ROOT]/CoinCod/Login_Form/login_form.php");
 			}
 		?>
 </body>
