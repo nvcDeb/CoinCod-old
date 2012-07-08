51a81217b9e12073a2e40d2cdec204f6a8afe242
vincent <vincent@nexvend.com>
2012-07-08 18:32:43 +0800

    the login change to index and path modify

--- Login_Form/check_login.php
+++ Login_Form/check_login.php
@@ -1,7 +1,10 @@
-<!DOCTYPE html">
-<html>
+<?php
+	session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>	
-<meta charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 
 <body>
@@ -38,7 +41,7 @@
 					<div id="after_log_in">
 					<div class="user_name">
 					<img src="'.$grav_url.'" alt="" />
-						<a href="../User_Profile/user_profile.php?id=' . $userid . '">' . $username . '
+						<a href="../User_Profile/?id=' . $userid . '">' . $username . '
 						</a>
 					</div>
 					
