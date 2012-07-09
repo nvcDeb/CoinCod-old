e626d337b28da94cd07bdf93b111b7562535eacc
Hilary Ho <hilary@nexvend.com>
2012-07-09 23:35:52 +0800

    remove unused div tags

--- Career/index.php
+++ Career/index.php
@@ -13,14 +13,14 @@
     <?php
 		include "../template/templateheader.php";
 	?>
-    <section div id="content_container">
+    <section id="content_container">
 	<h1><img src="../template/template_image/header/careers.png"></h1>
-    		<section div class="auction_container">					
+    		<section class="auction_container">					
 		Sorry, currently there are no available job openings. Please check with us again soon and hopefully we can provide you one in coming few months time.
 
 		<h1><img src="../template/template_image/bottom/careers.png"></h1>
-	  		</div></section>  <!--end div auction_container-->
-	</div></section>  <!--end div content_container-->
+	  		</section>  <!--end div auction_container-->
+	</section>  <!--end div content_container-->
 </div>  <!--wrapper-->
 
 <?php
