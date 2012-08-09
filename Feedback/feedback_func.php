63ccc6c3184782fda795f6f4a8868ed5d77ec24f
Hilary Ho <hilary@nexvend.com>
2012-08-10 01:30:20 +0800

    Fixes for doctype
    metatags eliminated and doctype fixed

--- Feedback/feedback_func.php
+++ Feedback/feedback_func.php
@@ -2,10 +2,10 @@
 	session_start();
 	include "../server.php";
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta charset="utf-8" />
 	<title>Feedback</title>
 </head>
 <body>
