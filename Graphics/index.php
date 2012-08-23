1237a29a92c8a49d42e96f4fa60393a7e3d2623d
vincent <vincent@nexvend.com>
2012-08-24 00:34:30 +0800

    repair the graphics code

--- Graphics/index.php
+++ Graphics/index.php
@@ -1,10 +1,11 @@
 <?php
-session_start();
+	session_start();
+	include "../config.php";
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="utf-8" />
 <title>Graphics</title>
 </head>
 
@@ -13,29 +14,29 @@ session_start();
     <?php
 		include "../template/templateheader.php";
 	?>
-	<div id="content_container">
+	<section id="content_container">
 			<h5>Graphics</h5>
-		<div class="auction_container">
+		<section class="auction_container">
 		
-		<div id="graphics">
+		<section id="graphics">
 			
-			<img src="../template/template_image/bottom/buy_tokens.png"><a href="download?file=buy_tokens.png">Download</a>
-			<img src="../template/template_image/bottom/careers.png"><a href="download?file=careers.png">Download</a>
-			<img src="../template/template_image/bottom/contacts.png"><a href="download?file=contacts.png">Download</a>
-			<img src="../template/template_image/bottom/faq.png"><a href="download?file=faq.png">Download</a>
-			<img src="../template/template_image/bottom/graphics.png"><a href="download?file=graphics.png">Download</a>
-			<img src="../template/template_image/bottom/how_it_works.png"><a href="download?file=how_it_works.png">Download</a>
-			<img src="../template/template_image/bottom/location.png"><a href="download?file=location.png">Download</a>
-			<img src="../template/template_image/bottom/privacy.png"><a href="download?file=privacy.png">Download</a>
-			<img src="../template/template_image/bottom/security.png"><a href="download?file=security.png">Download</a>
-			<img src="../template/template_image/bottom/site_status.png"><a href="download?file=site_status.png">Download</a>
-			<img src="../template/template_image/bottom/sorry.png"><a href="download?file=sorry.png">Download</a>
-			<img src="../template/template_image/bottom/term_of_service.png"><a href="download?file=term_of_service.png">Download</a>
+			<img src="../template/template_image/bottom/buy_tokens.png"><a href="graphics?file=buy_tokens.png">Download</a>
+			<img src="../template/template_image/bottom/careers.png"><a href="graphics?file=careers.png">Download</a>
+			<img src="../template/template_image/bottom/contacts.png"><a href="graphics?file=contacts.png">Download</a>
+			<img src="../template/template_image/bottom/faq.png"><a href="graphics?file=faq.png">Download</a>
+			<img src="../template/template_image/bottom/graphics.png"><a href="graphics?file=graphics.png">Download</a>
+			<img src="../template/template_image/bottom/how_it_works.png"><a href="graphics?file=how_it_works.png">Download</a>
+			<img src="../template/template_image/bottom/location.png"><a href="graphics?file=location.png">Download</a>
+			<img src="../template/template_image/bottom/privacy.png"><a href="graphics?file=privacy.png">Download</a>
+			<img src="../template/template_image/bottom/security.png"><a href="graphics?file=security.png">Download</a>
+			<img src="../template/template_image/bottom/site_status.png"><a href="graphics?file=site_status.png">Download</a>
+			<img src="../template/template_image/bottom/sorry.png"><a href="graphics?file=sorry.png">Download</a>
+			<img src="../template/template_image/bottom/term_of_service.png"><a href="graphics?file=term_of_service.png">Download</a>
 			
-		</div>	
-		</div>
+		</section>	
+		</section>
 	 
-	</div>
+	</section>
 </div><!--wrapper-->
 	<?php
 		include "../template/templatefooter.html";
