19701a833a091920221336dcab93d2ffb93cbef9
vincent <vincent@nexvend.com>
2012-08-23 23:28:29 +0800

    the graphic page updated

--- Graphics/index.php
+++ Graphics/index.php
@@ -1,11 +1,10 @@
 <?php
-	session_start();
-	include "../config.php";
+session_start();
 ?>
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Graphics</title>
 </head>
 
@@ -14,15 +13,29 @@
     <?php
 		include "../template/templateheader.php";
 	?>
-	<section id="content_container">
+	<div id="content_container">
 			<h5>Graphics</h5>
-		<section class="auction_container">
-			<img src="<?php echo $PREFIX; ?>/template/template_image/Coming-soon.gif" width="540px" height="380px"><p>
+		<div class="auction_container">
+		
+		<div id="graphics">
 			
-			<h1><img src="<?php echo $PREFIX; ?>/template/template_image/bottom/sorry.png"></h1>
-		</section>
+			<img src="../template/template_image/bottom/buy_tokens.png"><a href="graphics?file=buy_tokens.png">Download</a>
+			<img src="../template/template_image/bottom/careers.png"><a href="graphics?file=careers.png">Download</a>
+			<img src="../template/template_image/bottom/contacts.png"><a href="graphics?file=contacts.png">Download</a>
+			<img src="../template/template_image/bottom/faq.png"><a href="graphics?file=faq.png">Download</a>
+			<img src="../template/template_image/bottom/graphics.png"><a href="graphics?file=graphics.png">Download</a>
+			<img src="../template/template_image/bottom/how_it_works.png"><a href="graphics?file=how_it_works.png">Download</a>
+			<img src="../template/template_image/bottom/location.png"><a href="graphics?file=location.png">Download</a>
+			<img src="../template/template_image/bottom/privacy.png"><a href="graphics?file=privacy.png">Download</a>
+			<img src="../template/template_image/bottom/security.png"><a href="graphics?file=security.png">Download</a>
+			<img src="../template/template_image/bottom/site_status.png"><a href="graphics?file=site_status.png">Download</a>
+			<img src="../template/template_image/bottom/sorry.png"><a href="graphics?file=sorry.png">Download</a>
+			<img src="../template/template_image/bottom/term_of_service.png"><a href="graphics?file=term_of_service.png">Download</a>
+			
+		</div>	
+		</div>
 	 
-	</section>
+	</div>
 </div><!--wrapper-->
 	<?php
 		include "../template/templatefooter.html";
