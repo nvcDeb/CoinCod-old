81814859ff8778e3ef299d36672c659407fbe99f
Wilson <wilson@nexvend.com>
2012-09-11 11:19:44 +0800

    update paypal

--- Buy_Tokens/index.php
+++ Buy_Tokens/index.php
@@ -51,19 +51,16 @@ include "../config.php";
                 
 					<ul class="token_nav">
 						<h4>Paypal (RM)</h4>
-							
 								<?php 
 								$array = array("D83KWGJ6FX9YA"=> 15, "X24J6YZZDUN48"=> 30,"BXDZPNG8VAQCA"=>60,"QBD5BC7F7S5YW"=>"142.50","QRQHHTB6L7AK2"=>270,"VGJA9EG9CZ2QJ"=>600,"AZR4JZBCWDX2L"=>1125);
 								foreach ( $array as $i => $j) {
 									echo '
-										<div class="price_button">
 										<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-										<input type="hidden" name="cmd" value="_s-xclick">
-										<input type="hidden" name="hosted_button_id" value="'.$i.'">        
-										<input type="image" src="http://coincod.com/template/price_button/'.$j.'.png" width="104" height="34" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-										<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+											<input type="hidden" name="cmd" value="_s-xclick">
+											<input type="hidden" name="hosted_button_id" value="'.$i.'">        
+											<input type="image" src="http://auction12345.site50.net/template/price_button/'.$j.'.png" width="104" height="34" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+											<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 										</form>
-										</div>
 									';
 								} 
 							?>
