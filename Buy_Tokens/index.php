49c31e16836978673e6a731811b0b680c7cf09b8
nvcWilson <wilson@nexvend.com>
2012-09-11 11:22:36 +0800

    Update Buy_Tokens/index.php

--- Buy_Tokens/index.php
+++ Buy_Tokens/index.php
@@ -58,7 +58,7 @@ include "../config.php";
 										<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 											<input type="hidden" name="cmd" value="_s-xclick">
 											<input type="hidden" name="hosted_button_id" value="'.$i.'">        
-											<input type="image" src="http://auction12345.site50.net/template/price_button/'.$j.'.png" width="104" height="34" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+											<input type="image" src="http://coincod.com/template/price_button/'.$j.'.png" width="104" height="34" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 											<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 										</form>
 									';
