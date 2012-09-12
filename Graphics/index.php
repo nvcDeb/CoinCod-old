7c9e5d8dce06e2ac6d3fb5e87451bab48fcb40e4
vincent <vincent@nexvend.com>
2012-09-12 23:16:51 +0800

    the adding of alt in graphics page

--- Graphics/index.php
+++ Graphics/index.php
@@ -20,18 +20,18 @@
 		
 		<section id="graphics">
 			
-			<img src="../template/template_image/bottom/buy_tokens.png" alt="tokens shopping"><a href="download?file=buy_tokens.png">Download</a>
-			<img src="../template/template_image/bottom/careers.png" alt="careers"><a href="download?file=careers.png">Download</a>
-			<img src="../template/template_image/bottom/contacts.png" alt="contact us"><a href="download?file=contacts.png">Download</a>
-			<img src="../template/template_image/bottom/faq.png" alt="questions"><a href="download?file=faq.png">Download</a>
-			<img src="../template/template_image/bottom/graphics.png" alt="drawing"><a href="download?file=graphics.png">Download</a>
-			<img src="../template/template_image/bottom/how_it_works.png" alt="how works"><a href="download?file=how_it_works.png">Download</a>
-			<img src="../template/template_image/bottom/location.png" alt="pointer"><a href="download?file=location.png">Download</a>
-			<img src="../template/template_image/bottom/privacy.png" alt="lock"><a href="download?file=privacy.png">Download</a>
-			<img src="../template/template_image/bottom/security.png" alt="shield"><a href="download?file=security.png">Download</a>
-			<img src="../template/template_image/bottom/site_status.png" alt="communicating"><a href="download?file=site_status.png">Download</a>
-			<img src="../template/template_image/bottom/sorry.png" alt="sorry look"><a href="download?file=sorry.png">Download</a>
-			<img src="../template/template_image/bottom/term_of_service.png" alt="focus"><a href="download?file=term_of_service.png">Download</a>
+			<img src="../template/template_image/bottom/buy_tokens.png"><a href="graphics?file=buy_tokens.png">Download</a>
+			<img src="../template/template_image/bottom/careers.png"><a href="graphics?file=careers.png">Download</a>
+			<img src="../template/template_image/bottom/contacts.png"><a href="graphics?file=contacts.png">Download</a>
+			<img src="../template/template_image/bottom/faq.png"><a href="graphics?file=faq.png">Download</a>
+			<img src="../template/template_image/bottom/graphics.png"><a href="graphics?file=graphics.png">Download</a>
+			<img src="../template/template_image/bottom/how_it_works.png"><a href="graphics?file=how_it_works.png">Download</a>
+			<img src="../template/template_image/bottom/location.png"><a href="graphics?file=location.png">Download</a>
+			<img src="../template/template_image/bottom/privacy.png"><a href="graphics?file=privacy.png">Download</a>
+			<img src="../template/template_image/bottom/security.png"><a href="graphics?file=security.png">Download</a>
+			<img src="../template/template_image/bottom/site_status.png"><a href="graphics?file=site_status.png">Download</a>
+			<img src="../template/template_image/bottom/sorry.png"><a href="graphics?file=sorry.png">Download</a>
+			<img src="../template/template_image/bottom/term_of_service.png"><a href="graphics?file=term_of_service.png">Download</a>
 			
 		</section>	
 		</section>
