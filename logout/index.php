f100c18c67b2f74964f7e5bf88488979efc198e1
vincent <vincent@nexvend.com>
2012-10-07 22:34:15 +0800

    removal of capital

---
+++ logout/index.php
@@ -0,0 +1,23 @@
+<?php
+	ob_start();
+	include "../config.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" HTTP-EQUIV="Refresh" CONTENT="0;URL=<?php echo $PREFIX; ?>"/>
+	<title>Logout</title>
+</head>
+
+<body>
+	<div id="wrapper">
+	<?php
+		session_start();
+		session_destroy();
+		unset( $_SESSION );
+		
+	</div>  <!--end div wrapper-->
+	?>
+        
+</body>
+</html>
